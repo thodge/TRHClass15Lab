b5eab5a42bbe108565f72e5c6a2e95b7ab7f9909
Tammie <Tammie@10.30.19.174>
2014-03-24 19:14:01 -0500

    1

---
+++ src/trhclasss15lab/Invoice.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package trhclasss15lab;
+
+import java.util.Calendar;
+
+/**
+ *
+ * @author Tammie
+ */
+public class Invoice {
+    private Calendar invoiceDate;
+    private int gracePeriod = 15;
+    private Calendar dueDate;
+
+    public Invoice(Calendar invoiceDate) {
+        this.invoiceDate = invoiceDate;
+    }
+    public 
+    
+    
+    
+    
+    
+}
