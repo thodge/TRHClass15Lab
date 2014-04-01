981551440e6ff6097ab3a28f326d5bd7c2e8e58f
Tammie <Tammie@10.30.18.232>
2014-04-01 16:01:10 -0500

    3

--- src/trhclasss15lab/DateService.java
+++ src/trhclasss15lab/DateService.java
@@ -6,10 +6,20 @@
 
 package trhclasss15lab;
 
+import static com.sun.org.apache.xalan.internal.lib.ExsltDatetime.date;
+import java.util.Calendar;
+import java.util.Date;
+
 /**
  *
  * @author Tammie
  */
 public class DateService {
+    
+        Calendar c = Calendar.getInstance();
+        c.set(2000, Calendar.APRIL, 21);
+        System.out.println("3 -The date output via toString(): " + c);
+        date = c.getTime();
+        System.out.println("4 - The date output via toString(): " + date);
     
 }
